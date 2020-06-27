dd43f06d9c4685c6401cb38204293a7f8f9c1516
Adam Egerton <adam@egerton.me>
2020-06-27 17:22:34 +0100

    variables and strings

---
+++ strings and inputs.py
@@ -0,0 +1,11 @@
+animal = 'elephant'
+colour = 'pink'
+dream = colour+animal
+dream = colour + ' ' + animal
+print(dream)
+
+name = input( 'What is your name? ')
+initial = name[0]
+
+s = ' David James Smith '
+m = s[7:12]
---
+++ variables.py
@@ -0,0 +1,9 @@
+rabbits = 126
+carrots = 0
+print(rabbits)
+rabbits = 150
+print(rabbits)
+carrots = rabbits
+print(rabbits, carrots)
+rabbits = 250
+print(rabbits, carrots)
