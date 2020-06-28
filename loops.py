94a47cfeb149336be1fcd7d334e1e722f676faa8
Adam Egerton <adam@egerton.me>
2020-06-28 13:50:47 +0100

    loops

---
+++ loops.py
@@ -0,0 +1,16 @@
+for n in range(8):
+    print('n', n)
+for n in range(1, 13):
+    print('n', n)
+for n in range(3, 100, 3):
+    print('n', n)
+for n in range(10, 0, -1):
+    print(n)
+print("BLAST OFF!")
+
+from turtle import *
+
+for n in range(4):
+    forward(100)
+    right(90)
+
